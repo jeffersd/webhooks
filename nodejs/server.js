ae1d443374a44aa40925e5f4b2bfe33b1895705f
Dillon Jeffers <dillon_jeffers@fastmail.com>
2021-02-17 10:12:54 +0000

    add environment checks to the server file

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,4 +1,13 @@
 const PORT = process.env.WEBHOOK_PORT;
 
+if (!PORT) {
+    console.error("WEBHOOK_PORT not set");
+    process.exit(1);
+}
+if (!process.env.WEBHOOK_SECRET) {
+    console.error("WEBHOOK_SECRET not set");
+    process.exit(1);
+}
+
 console.log("listening on port:", PORT);
 require("./index.js").listen(PORT);
