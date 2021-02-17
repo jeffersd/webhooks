021f40f6ff625b736bca770e5011f47325e9baa6
Dillon Jeffers <dillon_jeffers@fastmail.com>
2021-02-17 10:26:27 +0000

    add a pm2 ecosystem file
    This will help us manage our webhook server as a system process.

---
+++ nodejs/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [{
+    name: 'webhooks',
+    script: 'node server.js',
+    watch: ['./index.js', './server.js'],
+    env: {
+        'NODE_ENV': 'development',
+        WEBHOOK_PORT: 3000,
+    },
+    'env_production': {
+        'NODE_ENV': 'production',
+        WEBHOOK_PORT: 5050,
+    },
+    'max_restarts': 3,
+    'restart_delay': 4000,
+  }],
+};
