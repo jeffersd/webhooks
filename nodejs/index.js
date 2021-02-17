18b3bfa6e5cc5191ec7cdf72f118dc782a38d68d
Dillon Jeffers <dillon_jeffers@fastmail.com>
2021-02-17 07:48:06 +0000

    add a test for bad json data
    use the 'end' event instead of the 'close' even to properly handle responses
    
    refactor an if guard before the data event handler

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,18 +3,28 @@ const server = require("http").createServer();
 server.on("request", (req, res) => {
     let requestContent = "";
 
-    req.on("data", (chunk) => {
-        requestContent += chunk;
-    });
-    req.on("close", () => {
-        console.log("request content:", requestContent);
-    });
     if (req.method !== "POST" || req.url !== "/webhook") {
         res.writeHead(404);
         return res.end("Not Found");
     }
-    res.writeHead(200);
-    return res.end("OK");
+
+    req.on("data", (chunk) => {
+        requestContent += chunk;
+    });
+    req.on("end", () => {
+        let parsed;
+
+        console.log("request content:", requestContent);
+        try {
+            parsed = JSON.parse(requestContent);
+        } catch (parseError) {
+            console.error(parseError);
+            res.writeHead(400);
+            return res.end("Bad json");
+        }
+        res.writeHead(200);
+        return res.end("OK");
+    });
 });
 
 module.exports = server;
--- nodejs/test/index.js
+++ nodejs/test/index.js
@@ -18,6 +18,14 @@ test("should return a 404 for a POST to /", () => {
         .expect("Not Found");
 });
 
+test("should return a 400 for POSTing invalid Json to /webhook", () => {
+    return request(webhook)
+        .post("/webhook")
+        .send("data")
+        .expect(400)
+        .expect("Bad json");
+});
+
 test("should return a 200 for a POST to /webhook", () => {
     return request(webhook)
         .post("/webhook")
