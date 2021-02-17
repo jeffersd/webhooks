635f72d6d64e17b35a53b2a36fc3f3ab0fc0994a
Dillon Jeffers <dillon_jeffers@fastmail.com>
2021-02-17 06:54:37 +0000

    process requests in the node server

--- node.js
+++ node.js
@@ -1,5 +1,27 @@
-require("http").createServer((req, res) => {
-    req.writeHead(200, { "Content-Type": "text/plain" });
-    return res.end("hello world");
-}).listen(3000);
+const server = require("http").createServer(),
+  PORT = 3000;
 
+server.on("request", (req, res) => {
+    let requestContent = "";
+
+    req.on("data", (chunk) => {
+        console.log("data");
+        requestContent += chunk;
+    });
+    req.on("aborted", (err) => {
+        console.error("aborted:", err);
+    });
+    req.on("close", () => {
+        console.error("req closed");
+
+        console.log("method:", req.method);
+        console.log("url:", req.url);
+        console.log("raw headers:", req.rawHeaders);
+        console.log("request content:", requestContent);
+    });
+    res.writeHead(200);
+    return res.end();
+});
+
+console.log("listening on port:", PORT);
+server.listen(PORT);
